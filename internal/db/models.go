f8a9e62c5f8c461bf06d17825da104ded4126bd1
thearturca <arti.1046@gmail.com>
2024-07-30 11:32:25 +0300

    fix: text gets emptied after processing

--- internal/db/models.go
+++ internal/db/models.go
@@ -6,7 +6,7 @@ import (
 
 type Message struct {
 	Id          string     `db:"id" json:"id" goqu:"pk,skipupdate,skipinsert,defaultifempty"`
-	Text        string     `db:"text" json:"text"`
+	Text        string     `db:"text" json:"text" goqu:"defaultifempty"`
 	IsProcessed bool       `db:"is_processed" json:"isProcessed" goqu:"skipinsert,defaultifempty"`
 	ProcessedAt *time.Time `db:"processed_at" json:"processedAt" goqu:"defaultifempty"`
 	CreatedAt   time.Time  `db:"created_at" json:"createdAt" goqu:"skipupdate,skipinsert,defaultifempty"`
